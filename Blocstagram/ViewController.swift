81f9e921088ab937f8ef32b6ae6f4252cb1d528d
Jin Kato <jin.kato@starcomww.com>
2015-11-28 13:35:03 -0500

    first commit

---
+++ Blocstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blocstagram
+//
+//  Created by Jin Kato on 11/28/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
