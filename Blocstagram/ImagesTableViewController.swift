d1b493c9f7b1d7da04ef64c481c3e59e0cfca91e
Jin Kato <jin.kato@starcomww.com>
2015-11-29 14:31:22 -0500

    27 tutorial

---
+++ Blocstagram/ImagesTableViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ImagesTableViewController.swift
+//  Blocstagram
+//
+//  Created by Jin Kato on 11/28/15.
+//  Copyright © 2015 Jin Kato. All rights reserved.
+//
+
+import UIKit
+
+class ImagesTableViewController: UITableViewController {
+
+    var images : [String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for i in 1...10 {
+            let imageName : String = "\(i).jpg";
+            images.append(imageName)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.images.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("imageCell", forIndexPath: indexPath) as UITableViewCell
+        let imageViewObject :UIImageView = UIImageView()
+        let w = cell.frame.width
+        let h = cell.frame.height
+        
+        imageViewObject.frame = CGRect(x: 0, y: 0, width: w, height: h)
+        //imageViewObject.autoresizingMask = UIViewAutoresizing.FlexibleHeight
+        //imageViewObject.autoresizingMask = UIViewAutoresizing.FlexibleWidth
+        imageViewObject.image = UIImage(named:images[indexPath.row])
+        cell.addSubview(imageViewObject)
+
+        return cell
+    }
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        let image = UIImage(named: images[indexPath.row])
+        let sizeOfImage = image?.size
+        let outerViewWidth = self.view.frame.width
+        let imageWidth = sizeOfImage!.width
+        let imageHeight = sizeOfImage!.height
+        return CGFloat((outerViewWidth/imageWidth) * imageHeight)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
